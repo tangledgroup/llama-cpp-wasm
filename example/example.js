1fe0c55515baacb2e801f4a0d37e8215b3de7923
Marko Tasic <mtasic85@gmail.com>
2024-02-11 21:44:48 +0100

    example: use picocss and highlightjs

--- example/example.js
+++ example/example.js
@@ -1,28 +1,38 @@
 import { LlamaCpp } from "./llama/llama.js";
 
-const onModelLoaded = () => { 
-    console.debug('model: loaded');
-    const prompt = document.querySelector("#prompt").value;
-    document.querySelector("#result").value = prompt;
+let app;
+const buttonRun = document.getElementById("run");
+const modelProgress = document.getElementById("model-progress");
 
-    app.run({
-        prompt: prompt,
-        ctx_size: 4096,
-        temp: 0.1,
-        no_display_prompt: true,
-    });
+const onModelLoaded = () => { 
+  console.debug('model: loaded');
+  const prompt = document.querySelector("#prompt").value;
+  document.querySelector("#result").value = prompt;
+
+  app.run({
+      prompt: prompt,
+      ctx_size: 4096,
+      temp: 0.1,
+      no_display_prompt: true,
+  });
 }
 
 const onMessageChunk = (text) => {
-    console.log(text);
-    document.querySelector('#result').value += text;
+  console.log(text);
+  document.querySelector('#result').value += text;
 };
 
 const onComplete = () => {
-    console.debug('model: completed');
+  console.debug('model: completed');
+  modelProgress.setAttribute('hidden', 'hidden');
+  buttonRun.removeAttribute('hidden');
 };
 
-const app = new LlamaCpp(
+buttonRun.addEventListener("click", (e) => {
+  modelProgress.removeAttribute('hidden');
+  buttonRun.setAttribute('hidden', 'hidden');
+
+  app = new LlamaCpp(
     // 'https://huggingface.co/Qwen/Qwen1.5-0.5B-Chat-GGUF/resolve/main/qwen2-beta-0_5b-chat-q8_0.gguf',
     // 'https://huggingface.co/Qwen/Qwen1.5-1.8B-Chat-GGUF/resolve/main/qwen1_5-1_8b-chat-q8_0.gguf',
     'https://huggingface.co/stabilityai/stablelm-2-zephyr-1_6b/resolve/main/stablelm-2-zephyr-1_6b-Q4_1.gguf',
@@ -31,4 +41,5 @@ const app = new LlamaCpp(
     onModelLoaded,          
     onMessageChunk,       
     onComplete,
-);
+  );
+});
