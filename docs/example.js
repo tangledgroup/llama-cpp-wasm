42abfb48e636186924369a03cbbda6367e2f44c5
Marko Tasic <mtasic85@gmail.com>
2024-02-12 12:34:43 +0100

    renamed example to docs

---
+++ docs/example.js
@@ -0,0 +1,59 @@
+import { LlamaCpp } from "./llama/llama.js";
+
+let app;
+const buttonRun = document.querySelector("#run");
+const buttonRunProgressLoadingModel = document.querySelector("#run-progress-loading-model");
+const buttonRunProgressLoadedModel = document.querySelector("#run-progress-loaded-model");
+const buttonRunProgressGenerating = document.querySelector("#run-progress-generating");
+const selectModel = document.querySelector("select#model");
+const modelProgress = document.querySelector("#model-progress");
+const textareaPrompt = document.querySelector("textarea#prompt");
+const textareaResult = document.querySelector("textarea#result");
+
+const onModelLoaded = () => {
+  const prompt = textareaPrompt.value;
+  buttonRunProgressLoadingModel.setAttribute("hidden", "hidden");
+  buttonRunProgressLoadedModel.removeAttribute("hidden");
+  console.debug("model: loaded");
+  
+  app.run({
+      prompt: prompt,
+      ctx_size: 4096,
+      temp: 0.1,
+      no_display_prompt: true,
+  });
+}
+
+const onMessageChunk = (text) => {
+  console.log(text);
+
+  if (buttonRunProgressGenerating.hasAttribute("hidden")) {
+    buttonRunProgressLoadingModel.setAttribute("hidden", "hidden");
+    buttonRunProgressLoadedModel.setAttribute("hidden", "hidden");
+    buttonRunProgressGenerating.removeAttribute("hidden");
+  }
+
+  textareaResult.value += text;
+};
+
+const onComplete = () => {
+  console.debug("model: completed");
+  buttonRun.removeAttribute("hidden");
+  buttonRunProgressLoadingModel.setAttribute("hidden", "hidden");
+  buttonRunProgressLoadedModel.setAttribute("hidden", "hidden");
+  buttonRunProgressGenerating.setAttribute("hidden", "hidden");
+  modelProgress.setAttribute("hidden", "hidden");
+};
+
+buttonRun.addEventListener("click", (e) => {
+  buttonRun.setAttribute("hidden", "hidden");
+  buttonRunProgressLoadingModel.removeAttribute("hidden");
+  modelProgress.removeAttribute("hidden");
+
+  app = new LlamaCpp(
+    selectModel.value,
+    onModelLoaded,          
+    onMessageChunk,       
+    onComplete,
+  );
+});
